0346b89ee802b934df5cba17c8b62350189b7dae
DaoVietDuong <rickmansro@gmail.com>
2016-01-30 17:45:35 +0700

    Ver 2
    1

--- app/app.js
+++ app/app.js
@@ -113,16 +113,16 @@ function($windowProvider, $stateProvider, $httpProvider , $urlRouterProvider, $r
         templateUrl : 'app/views/groupDocument.html',
         controller: 'grovDocumentController'         
     });        
-    if(window.history && window.history.pushState){
-            //$locationProvider.html5Mode(true); will cause an error $location in HTML5 mode requires a  tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
+    // if(window.history && window.history.pushState){
+            // //$locationProvider.html5Mode(true); will cause an error $location in HTML5 mode requires a  tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
 
-         // to know more about setting base URL visit: https://docs.angularjs.org/error/$location/nobase
+         // // to know more about setting base URL visit: https://docs.angularjs.org/error/$location/nobase
 
-         // if you don't wish to set base URL then use this
-         $locationProvider.html5Mode({
-                 enabled: false,
-                 requireBase: false
-          });
-        }
+         // // if you don't wish to set base URL then use this
+         // $locationProvider.html5Mode({
+                 // enabled: false,
+                 // requireBase: false
+          // });
+        // }
 }])
 
