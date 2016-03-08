656aa0e15244f9d2093ba3fb7fa3ce02b623622e
DaoVietDuong <rickmansro@gmail.com>
2016-03-08 11:49:07 +0700

    Final 8-3

--- www/app/filters/imgFilter.js
+++ www/app/filters/imgFilter.js
@@ -1,13 +1,12 @@
-ngFilters.filter("imgUrlFilter", function () {
-    return function (x) {
-        if (x) {
-            var n = x.indexOf('"', 10);
-            var m = x.indexOf('"')
-            var st = x.substring(m + 1, n);
-            var rootUrl = "http://bd.btsoftvn.net";
-            var result = rootUrl + st;
-            return result;
-        }
-        return "";
+ngFilters.filter("imgUrlFilter", function() {
+    return function(x) {
+        var n = x.indexOf('"', 10);
+        var m = x.indexOf('"')
+        var st = x.substring(m + 1, n);
+        var rootUrl = "http://222.252.25.31:8080";
+        var result = rootUrl + st;
+        return result;
+
+
     };
 });
